ae72223f69bf2942d1acceabeb964bbd3ad583b5
Lou Broue <lobroue@z2r2p5.42lyon.fr>
2026-03-26 20:40:14 +0100

    00 in progress

---
+++ 00/ex2/ft_plot_area.py
@@ -0,0 +1,5 @@
+def ft_plot_area() -> None:
+    lengh = int(input("Enter lengh: "))
+    width = int(input("Enter width: "))
+    area = lengh * width
+    print(f"Plot area: {area}")
---
+++ 00/ex3/ft_harvest_total.py
@@ -0,0 +1,7 @@
+def ft_harvest_total() -> None:
+    d1 = int(input("Day 1 harvest: "))
+    d2 = int(input("Day 2 harvest: "))
+    d3 = int(input("Day 3 harvest: "))
+    total = d1 + d2 + d3
+    print(f"Total harvest: {total}")
+ft_harvest_total()
