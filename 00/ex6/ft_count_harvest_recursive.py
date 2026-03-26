f73806f71239d26cc7ae3dd7f4d132c40b4d55ef
Lou Broue <lobroue@z2r2p5.42lyon.fr>
2026-03-26 20:59:17 +0100

    okok

---
+++ 00/ex6/ft_count_harvest_recursive.py
@@ -0,0 +1,11 @@
+def ft_count_harvest_recursive(day: int, days: int) -> None:
+    if day <= days:
+        print (f"Day {day}")
+    if day <= days:
+        ft_count_harvest_recursive(day + 1, days)
+
+
+def main() -> None:
+    days = int(input("Days until harvest: "))
+    ft_count_harvest_recursive(1,days)
+    print("Harvest time")
