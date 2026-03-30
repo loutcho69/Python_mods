63f0e5d831738db172d6219831d8829dc7256442
Lou Broue <lobroue@z2r10p2.42lyon.fr>
2026-03-30 22:37:40 +0200

    flakeok 02

--- 02/ex0/ft_first_exception.py
+++ 02/ex0/ft_first_exception.py
@@ -1,6 +1,7 @@
 def input_temperature(temp_str: str) -> int:
     return int(temp_str)
 
+
 def test_temperature() -> None:
     print("=== Garden temperature ===")
     print("Input data is: '25'")
@@ -16,4 +17,3 @@ def test_temperature() -> None:
 
 if __name__ == "__main__":
     test_temperature()
-
--- 02/ex1/ft_raise_exception.py
+++ 02/ex1/ft_raise_exception.py
@@ -1,10 +1,11 @@
 def input_temperature(temp_str: str) -> int:
     res = int(temp_str)
     if res < 0:
-        raise ValueError (f"{res}°C is too cold for plants (min 0°C)")
+        raise ValueError(f"{res}°C is too cold for plants (min 0°C)")
     if res > 40:
-        raise ValueError (f"{res}°C is too hot for plants (max 40°C)")
+        raise ValueError(f"{res}°C is too hot for plants (max 40°C)")
     return int(temp_str)
+
 
 def test_temperature() -> None:
     print("=== Garden Temperature Checker")
@@ -20,12 +21,13 @@ def test_temperature() -> None:
         input_temperature("100")
     except ValueError as e:
         print(f"Cought input_temperature error: {e}")
-    try: 
+    try:
         print("Input data is: '-50'")
         input_temperature("-50")
     except ValueError as e:
         print(f"Cought input_temperature error: {e}")
     print("All tests completed - program didn't crash!")
+
 
 if __name__ == "__main__":
     test_temperature()
