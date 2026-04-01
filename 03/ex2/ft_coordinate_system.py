a8b8e070cf730376eb540555bb6257ff049c108b
Lou Broue <lobroue@z2r12p6.42lyon.fr>
2026-04-01 20:21:34 +0200

    ex2 03 done

---
+++ 03/ex2/ft_coordinate_system.py
@@ -0,0 +1,46 @@
+import math
+
+
+def get_player_pos() -> tuple:
+    try:
+        x, y, z = input("Enter new coordinates as floats in format 'x,y,z': ")\
+            .split(",")
+        tmp = [x, y, z]
+    except Exception:
+        print("Invalid syntax")
+        get_player_pos()
+    pos = []
+    for i in tmp:
+        try:
+            nb = float(i)
+            pos.append(nb)
+        except ValueError as e:
+            print(f"Error on parameter {i}: {e}")
+            get_player_pos()
+    return (pos[0], pos[1], pos[2])
+
+
+def get_distance(pos1: tuple, pos2: tuple) -> float:
+    x1, y1, z1 = pos1
+    x2, y2, z2 = pos2
+    return math.sqrt((x2-x1) ** 2 + (y2-y1) ** 2 + (z2-z1) ** 2)
+
+
+def main() -> None:
+    print("=== Game Coordinate System ===")
+    x, y, z = 0.0, 0.0, 0.0
+    defpos = (x, y, z)
+    pos1 = get_player_pos()
+    print(f"Got a first tuple: {pos1}")
+    print(f"It includes: X={pos1[0]}, Y={pos1[1]}, Z={pos1[2]}")
+    print(f"Distance to center: {round(get_distance(pos1, defpos), 4)}\n")
+    print("Get a second set of coordinates")
+    pos2 = get_player_pos()
+    print(
+        f"Distance between the 2 sets of coordinates:"
+        f"{round(get_distance(pos1, pos2), 4)} "
+    )
+
+
+if __name__ == "__main__":
+    main()
